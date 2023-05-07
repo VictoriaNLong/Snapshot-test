11b4bb5e904cdc95a98dd22b430385d2676e0923
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-05-06 19:02:44 -0700

    fin

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Newer Test</h2>
+        <img src={logo} className="App-logo" alt="logo for game" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
